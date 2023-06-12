be659e1311580220a9c7321302a2309ac94f070e
kiko <cahenriquesoares@gmail.com>
2023-06-12 16:56:30 -0300

    Implementa listagem de tarefas pendentes

--- todolist/tarefas/views.py
+++ todolist/tarefas/views.py
@@ -3,13 +3,16 @@ from django.shortcuts import HttpResponseRedirect
 from todolist.tarefas.forms import TarefaNovaForm
 from django.urls import reverse
 
+from todolist.tarefas.models import Tarefa
+
 
 def home(request):
+    tarefas_pendentes = Tarefa.objects.filter(feita=False).all()
     if request.method == 'POST':
         form = TarefaNovaForm(request.POST)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse('tarefas:home'))
         else:
-            return render(request, 'tarefas/home.html', {'form': form}, status=400)
-    return render(request, 'tarefas/home.html')
+            return render(request, 'tarefas/home.html', {'form': form, 'tarefas_pendentes': tarefas_pendentes}, status=400)
+    return render(request, 'tarefas/home.html', {'tarefas_pendentes': tarefas_pendentes})
