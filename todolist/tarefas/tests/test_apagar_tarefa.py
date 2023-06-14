569f7fe749b0f80be6b5684c94ea28d3fbfcc3d1
kiko <cahenriquesoares@gmail.com>
2023-06-14 15:08:04 -0300

    Implementa funcionalidade de deletar tarefa

---
+++ todolist/tarefas/tests/test_apagar_tarefa.py
@@ -0,0 +1,20 @@
+import pytest
+from django.urls import reverse
+from todolist.tarefas.models import Tarefa
+
+
+@pytest.fixture
+def tarefa(db):
+    return Tarefa.objects.create(nome='Tarefa 1', feita=True)
+
+
+@pytest.fixture
+def resposta(client, tarefa):
+    resp = client.post(
+        reverse('tarefas:apagar', kwargs={'tarefa_id': tarefa.id})
+    )
+    return resp
+
+
+def test_apagar_tarefa(resposta):
+    assert not Tarefa.objects.exists()
